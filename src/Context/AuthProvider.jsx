84e346f84774fad006fbe34ff66766bd10aa33d9
himel1625 <himelmia1625@gmail.com>
2025-01-14 04:31:00 +0600

    implement private route

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,11 +1,16 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const AuthInfo = {
     PH: 'PH',
+    user,
+    loading,
   };
+
   return (
     <div>
       <AuthContext.Provider value={AuthInfo}>{children}</AuthContext.Provider>
